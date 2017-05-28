77e154b3208c8a304eb3d90ac5de5bc894f0ea02
CarloMicieli <carlo.micieli@gmail.com>
2017-05-28 18:29:11 +0200

    File header via sbt plugin

--- fp/src/main/scala/io/github/carlomicieli/fp/samples/transactions/Transaction.scala
+++ fp/src/main/scala/io/github/carlomicieli/fp/samples/transactions/Transaction.scala
@@ -4,9 +4,8 @@
  *   / ___/ ___/ __ `/ / __ `/_____/ __ `/ / __ `/ __ \
  *  (__  ) /__/ /_/ / / /_/ /_____/ /_/ / / /_/ / /_/ /
  * /____/\___/\__,_/_/\__,_/      \__,_/_/\__, /\____/
- *                                       /____/
- *
- * Copyright (c) 2015 the original author or authors.
+ *                   /____/
+ * Copyright (c) 2017 the original author or authors.
  * See the LICENCE.txt file distributed with this work for additional
  * information regarding copyright ownership.
  *
@@ -14,15 +13,15 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
+
 package io.github.carlomicieli.fp.samples.transactions
 
 sealed trait Transaction {
--- project/build.sbt
+++ project/build.sbt
@@ -3,3 +3,5 @@ resolvers += Classpaths.sbtPluginReleases
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.0.4")
 
 addSbtPlugin("org.scoverage" % "sbt-coveralls" % "1.0.0")
+
+addSbtPlugin("de.heikoseeberger" % "sbt-header" % "2.0.0")
